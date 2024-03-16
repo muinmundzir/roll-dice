8e319bea9e4a338068a1afc8ace30458045912a3
= <=>
2024-03-16 12:50:28 +0700

    refactor: modify widget to be reusable

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,25 +5,23 @@ class GradientContainer extends StatelessWidget {
   // const GradientContainer({key}) : super(key: key);
 
   // same as above
-  const GradientContainer({super.key});
+  const GradientContainer(this.colors, {super.key});
+
+  final List<Color> colors;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 25, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
+          colors: colors,
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText('Hello World!'),
       ),
     );
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice/gradient_container.dart';
 
+const colors = [
+  Color.fromARGB(255, 25, 2, 80),
+  Color.fromARGB(255, 45, 7, 98),
+];
+
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: GradientContainer()),
+      home: Scaffold(body: GradientContainer([...colors])),
     ),
   );
 }
