1b01a65f697191886a204ffcabc9180c937a3dfd
= <=>
2024-03-16 14:03:33 +0700

    feat: add dice roller stateful widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice/dice_roller.dart';
 
 class GradientContainer extends StatelessWidget {
   // const GradientContainer({key}) : super(key: key);
@@ -7,8 +8,6 @@ class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors, {super.key});
 
   final List<Color> colors;
-
-  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -20,25 +19,8 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-2.png',
-              width: 200,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.only(top: 20),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const Text('Roll Dice'),
-            )
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice/gradient_container.dart';
 
-const colors = [
-  Color.fromARGB(255, 25, 2, 80),
-  Color.fromARGB(255, 45, 7, 98),
+var colors = [
+  const Color.fromARGB(255, 25, 2, 80),
+  const Color.fromARGB(255, 45, 7, 98),
 ];
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(body: GradientContainer([...colors])),
     ),
   );
